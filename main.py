11a19975f47c1ca90371c8022574e6dd02403604
JoksiP <58534820+JoksiP@users.noreply.github.com>
2022-01-24 11:21:32 +0100

    Initialized all models and groundtruths.

--- main.py
+++ main.py
@@ -43,3 +43,46 @@ LABELS_YOLO = open("YOLO/coco.names.txt").read().strip().splitlines(keepends=Fal
 
 
 net = cv2.dnn.readNetFromDarknet(YOLO_CFG, YOLO_WEIGHTS)
+
+LABELS = LABELS_YOLO
+
+CONF = 0.3
+if args.get('conf'):
+    CONF = float(args.get('conf'))
+
+DISTANCE_RISK = float(args.get('min'))
+DISTANCE_POTENTIAL_RISK = float(args.get('max'))
+
+if args.get('net') == "ssd":
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT_SSD, MODEL_SSD)
+    LABELS = LABELS_SSD
+
+    # configure your ssd parameters here
+
+    if not args.get('conf'):
+        CONF = 0.4
+
+if args.get('net') == "yolo-tiny":
+    net = cv2.dnn.readNetFromDarknet(YOLO_TINY_CFG, YOLO_TINY_WEIGHTS)
+    LABELS = LABELS_TINY
+##########################################################################
+INP_VIDEO_PATH = 'TownCentreXVID.mp4'
+OUT_VIDEO_PATH = 'TownCentreXVIDDetected.mp4'
+
+GROUND_TRUTH = "TownCentre-groundtruth.top"
+
+gt = {}
+
+person_idx = LABELS.index("person")
+
+with open(GROUND_TRUTH, mode='r') as gf_file:
+    lines = gf_file.readlines()
+    for line in lines:
+        tokens = line.replace("\n", "").split(',')
+        tokens = [float(token) for token in tokens]
+        if int(tokens[1]) not in gt:
+            gt[int(tokens[1])] = []
+
+        gt[int(tokens[1])].append(
+            [int(tokens[-4]), int(tokens[-3]), int(tokens[-2]), int(tokens[-1]), person_idx, 0, 0]
+        )
